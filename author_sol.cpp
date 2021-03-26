10c8787d648b15656a48851a406fbbb2e40e53c1
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-03-26 18:47:04 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		// store the max speed of every car
+		vector<int> max_speed(n);
+		for (int i = 0; i < n; i++) {
+			cin >> max_speed[i];
+		}
+		// create another vector to calculate the current speed
+		vector<int> speed(n);
+		// the first car will always have its max speed
+		speed[0] = max_speed[0];
+		for (int i = 1; i < n; i++) {
+			// check if the current speed of the car is less than 
+			// the speed of the previous car, if its less than, then
+			// it will be unchanged, otherwise, the speed of the previous
+			// car will overwrite its speed
+			speed[i] = min(max_speed[i], speed[i - 1]);
+		}
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			// check if the current speed of every car matches its own max speed
+			if (speed[i] == max_speed[i]) {
+				cnt++;
+			}
+		}
+		cout << cnt << '\n';
+	}
+	return 0;
+}
